98ec9826ce045d0d8ca9fc3ef1efd987b588e0eb
mhlee-mozzet <mh.lee@mozzet.com>
2017-02-14 14:35:36 +0900

    SFSafariViewControllerCookiesTest

--- SFSafariViewControllerCookiesTest/AppDelegate.m
+++ SFSafariViewControllerCookiesTest/AppDelegate.m
@@ -7,8 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <SafariServices/SafariServices.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <SFSafariViewControllerDelegate>
 
 @end
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    SFSafariViewController *vc = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:@"https://developer.apple.com"]];
+    vc.delegate = self;
+    
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
@@ -47,5 +55,32 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+#pragma mark - SFSafariViewControllerDelegate
+
+/*! @abstract Called when the view controller is about to show UIActivityViewController after the user taps the action button.
+ @param URL the URL of the web page.
+ @param title the title of the web page.
+ @result Returns an array of UIActivity instances that will be appended to UIActivityViewController.
+ */
+//- (NSArray<UIActivity *> *)safariViewController:(SFSafariViewController *)controller activityItemsForURL:(NSURL *)URL title:(nullable NSString *)title {
+//    NSLog(@"%s URL: %@, title: %@", __FUNCTION__, URL, title);
+//    return nil;
+//}
+
+/*! @abstract Delegate callback called when the user taps the Done button. Upon this call, the view controller is dismissed modally. */
+- (void)safariViewControllerDidFinish:(SFSafariViewController *)controller {
+    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:[NSURL URLWithString:@"https://developer.apple.com"]];
+    NSLog(@"cookies: %@", cookies);
+}
+
+/*! @abstract Invoked when the initial URL load is complete.
+ @param success YES if loading completed successfully, NO if loading failed.
+ @discussion This method is invoked when SFSafariViewController completes the loading of the URL that you pass
+ to its initializer. It is not invoked for any subsequent page loads in the same SFSafariViewController instance.
+ */
+- (void)safariViewController:(SFSafariViewController *)controller didCompleteInitialLoad:(BOOL)didLoadSuccessfully {
+    NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:[NSURL URLWithString:@"https://developer.apple.com"]];
+    NSLog(@"cookies: %@", cookies);
+}
 
 @end
